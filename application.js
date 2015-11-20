b5e936a18fcc800560be0c743efddce12b86351f
Cole Roberts <cole.alan.roberts@gmail.com>
2015-11-20 01:22:51 -0600

    Build frontend

--- application.js
+++ application.js
@@ -7,31 +7,44 @@ $(function() {
     function stockArrayLen() {
         return stockArray.length;
     }
+
+    function formatCurrency (value) {
+        var suffixes = ["", "K", "M", "B","T"];
+        var suffixNum = Math.floor(("" + value).length / 3);
+        var shortValue = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(2));
+        if (shortValue % 1 != 0) {
+            shortNum = shortValue.toFixed(1);
+        }
+
+    return shortValue + suffixes[suffixNum];
+    }
     
-    l = stockArrayLen();
+    var l = stockArrayLen();
     console.log(l);
-    
-    function buildUI(d) {
+
+    function buildList(d) {
         var list = $('.stock__list');                
         var data = d;
         console.log(data);
+
+        var marketCap = formatCurrency(data.MarketCap);
         list.append(
-            '<li>'
-            + '<span class="stock__row">' + data.Name + '</span>'
+            '<li class="stock__company" data-symbol="' + data.Symbol +  '"' + '>'
+            + '<span class="stock__row stock__row--name">' + data.Name + '</span>'
             + ' ' 
-            + '<span class="stock__row">' + data.Symbol + '</span>'
+            + '<span class="stock__row stock__row--sym">' + data.Symbol + '</span>'
             + ' ' 
-            + '<span class="stock__row">' + data.MarketCap + '</span>'
+            + '<span class="stock__row stock__row--market-cap">' + '$' + marketCap + '</span>'
             + ' ' 
-            + '<span class="stock__row">' + data.ChangePercent + '</span>'
+            + '<span class="stock__row stock__row--change-pct">' +  '$' + data.Change.toFixed(2) + ' :: ' + data.ChangePercent.toFixed(2) + '%' + '</span>'
             + ' '
-            + '$'
-            + data.LastPrice 
+            + '<span class="stock__row stock__row--last-price>' + '$' + data.LastPrice 
             + '</li>');
     }
   
     function updatePrices() {
-      for (var i = 0, len = stockArray.length; i < len; i++) {                  getStockPrices(stockArray[i]);   
+      for (var i = 0, len = stockArray.length; i < len; i++) {                  
+        getStockPrices(stockArray[i]);   
       }
   }
 
@@ -39,19 +52,32 @@ $(function() {
     function getStockPrices(stock) {
         $.ajax({
             url: url + stock,
+            type: 'GET',
             contentType: 'application/json',
             dataType: 'jsonp'
         })
         .success(function(data) {
-            buildUI(data);
+            buildList(data);
+        })
+        .done(function() {
+            attachClickListeners();
         });
     }
-    
+
+    function getHistoryChart() {
+        var $el = $(this);
+        console.log($el.data('symbol'));
+    }
+
+    function attachClickListeners() {
+        $('.stock__company').unbind().click(getHistoryChart);
+    }
+
     // Run once
     updatePrices();
     
     setInterval(function() {
         updatePrices();
-    }, 60000);
+    }, 30000);
     
 });
